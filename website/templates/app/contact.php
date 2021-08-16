ab57b2093b8b020809159b349931c72c666ce076
maximematerno <maxime.materno@gmail.com>
2021-08-16 14:36:11 -0700

    modify files

--- website/templates/app/contact.php
+++ website/templates/app/contact.php
@@ -5,7 +5,9 @@ $message = $_POST['message'];
 
 $email_from ='Styles Restaurant Site';
 $email_subject = 'New Message From BETTERBETS';
-$email_body = "Name: $name.\n". "Email; $email.\n". "Message: $message.\n";
+$email_body = "Name: $name.\n". 
+              "Email; $email.\n". 
+              "Message: $message.\n";
 
 $to= 'maxime.materno@gmail.com';
 $headers= "From: $email_from \r\n";
